e785e5ed503732ff025bac8365f86bfce101f7d6
rika <rima361@naver.com>
2018-02-07 16:26:31 +0900

    login-router & 404 errorHandler

--- Server/login.js
+++ Server/login.js
@@ -1,22 +1,29 @@
-
 // login id 와 passoword 값 전송
 
 // Express 기본 모듈 부르기
 var express = require('express')
 , http = require('http')
 , path = require('path');
-
 // Express의 미들웨어 불러오기
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser') // ost 방식으로 사용할 수 있게끔 해줌
 , static = require('serve-static');
+// 오류 핸들러 모듈 사용
+var expressErrorHandler = require('express-error-handler');
 
 // Express 객체 생성 
 var app = express();
-app.set('port', process.env.PORT || 3000)
-
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('port', process.env.PORT || 3000);
+// use를 통해서 body-parser를 붙임, body-parser가 항상 대기하고 있다가 사용자가 post 던진 데이터를 사용할 수 있게끔 도와줌
+app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json());
-app.use(static(path.join('public')));
+// app.use(static(path.join(__dirname + '/public')));  // 이거 원리를 모르겠음 그리고 안돌아감 ???
+app.use(express.static('public'));
+
+// 로그인 화면으로 이동 
+app.get('/login', function(req, res){
+	// res.send("login please")
+	res.redirect('/login.html'); 
+})
 
 // 라우터 객체 참조
 var router = express.Router();
@@ -36,6 +43,17 @@ router.route('/process/login').post(function(req, res) {
 
 app.use('/', router);
 
+// 모든 router 처리 끝난 후 404 오류 페이지 처리
+// 서버를 시작하기 위해 호출하는 코드 위쪽에 추가 
+var errorHandler = expressErrorHandler({
+	static: {
+		'404' : './public/404.html'
+	}
+});
+app.use(expressErrorHandler.httpError(404));
+app.use(errorHandler);
+
 http.createServer(app).listen(3000, function(){
 	console.log('Express 서버가 3000번 포트에서 시작됨.');
 });
+
