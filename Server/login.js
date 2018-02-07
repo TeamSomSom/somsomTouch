4c05c2fe282a1613f2eee288b4536290f65d1e3f
rika <rima361@naver.com>
2018-02-07 17:44:26 +0900

    login session

--- Server/login.js
+++ Server/login.js
@@ -1,33 +1,33 @@
-// login id 와 passoword 값 전송
 
 // Express 기본 모듈 부르기
 var express = require('express')
 , http = require('http')
-, path = require('path');
-// Express의 미들웨어 불러오기
-var bodyParser = require('body-parser') // ost 방식으로 사용할 수 있게끔 해줌
-, static = require('serve-static');
-// 오류 핸들러 모듈 사용
-var expressErrorHandler = require('express-error-handler');
+, path = require('path')
+, bodyParser = require('body-parser')
+, static = require('serve-static')
+, expressErrorHandler = require('express-error-handler')
+, cookieParser = require('cookie-parser')
+, expressSession = require('express-session');
 
 // Express 객체 생성 
 var app = express();
 app.set('port', process.env.PORT || 3000);
-// use를 통해서 body-parser를 붙임, body-parser가 항상 대기하고 있다가 사용자가 post 던진 데이터를 사용할 수 있게끔 도와줌
+app.use(cookieParser());
+app.use(expressSession({
+    secret:'my key',
+    resave: true,
+    saveUninitialized:true
+}));
 app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json());
-// app.use(static(path.join(__dirname + '/public')));  // 이거 원리를 모르겠음 그리고 안돌아감 ???
 app.use(express.static('public'));
 
-// 로그인 화면으로 이동 
+// routing
+var router = express.Router();
 app.get('/login', function(req, res){
-	// res.send("login please")
 	res.redirect('/login.html'); 
 })
 
-// 라우터 객체 참조
-var router = express.Router();
-//라우팅 함수 등록
 router.route('/process/login').post(function(req, res) {
 	console.log('/process/login 처리함');
 
@@ -41,10 +41,41 @@ router.route('/process/login').post(function(req, res) {
 	res.end();
 });
 
+router.route('/process/showCookie').get(function(req, res) {
+	console.log('/process/showCookie 호출됨.');
+	res.send(req.cookies);
+ });
+
+ router.route('/process/setUserCookie').get(function(req, res) {
+	console.log('/process/setUserCookie 호출됨.');
+	
+	// 쿠키 설정
+	res.cookie('user', {
+	 id: 'mike',
+	 name: '소녀시대',
+	 authorized: true    
+	});
+ 
+	// redirect로 응답
+	res.redirect('/process/showCookie');
+ });
+
+ // session 
+ router.route('/').get(function(req, res){
+     console.log('/process/main 호출됨.');
+     
+     if(req.session.user) {
+        console.log('session 있음');
+         res.redirect('/index.html');
+     }
+     else {
+        console.log('session 없음');
+         res.redirect('/login.html');
+     }
+ });
 app.use('/', router);
 
-// 모든 router 처리 끝난 후 404 오류 페이지 처리
-// 서버를 시작하기 위해 호출하는 코드 위쪽에 추가 
+//error
 var errorHandler = expressErrorHandler({
 	static: {
 		'404' : './public/404.html'
@@ -53,7 +84,7 @@ var errorHandler = expressErrorHandler({
 app.use(expressErrorHandler.httpError(404));
 app.use(errorHandler);
 
+// server-start
 http.createServer(app).listen(3000, function(){
 	console.log('Express 서버가 3000번 포트에서 시작됨.');
 });
-
