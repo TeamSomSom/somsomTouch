1cf817e48825d1230e24ef878233a78d0fb6987b
LimAh Lee <rima361@naver.com>
2018-03-09 13:48:00 +0900

    회원가입 Passport

--- controller/userController.js
+++ controller/userController.js
@@ -8,25 +8,35 @@ var expressSession = require('express-session'),
 
 // Session
 var app = express();
+var store = redis.createClient({host:'localhost', port: 6379});
 
 /*********************************************************************** 
  *                              User Create  						   
 *************************************************************************/
 
-var store = redis.createClient({host:'localhost', port: 6379});
-var cnt = 0;
+var bkfd2Password = require("pbkdf2-password");
+var hasher = bkfd2Password();
+var assert = require("assert");
+
 exports.create = function(req, res) {
 	console.log('/process/signup 처리함');
-
-	var paramId = req.body.id;
-	var paramEmail = req.body.email;
-	var paramPassword = req.body.password;
+	hasher({password:req.body.password}, function(err, pass, salt, hash){
+		var paramId = req.body.id;
+		var paramEmail = req.body.email;
+		var paramPassword = hash;
+		var salt = salt;
+		
+		console.log('id: ' + paramId + ', password : ' + paramPassword + ', email: ' + paramEmail);
 	
-	console.log('id: ' + paramId + ', password : ' + paramPassword + ', email: ' + paramEmail);
-	
-	store.hmset("user:"+paramId, "pwd", paramPassword, "email", paramEmail);
-	res.redirect('/');
-};
+		store.hmset("user:"+paramId, "pwd", paramPassword, "email", paramEmail, "salt", salt);
+		
+		// 회원가입후 바로 로그인 시켜주기 
+		req.session.displayName = req.body.displayName;
+		req.session.save(function(){
+			res.redirect('/');
+		})
+	});
+}
 
 /*********************************************************************** 
  *                              User Update  						   
--- models/user.js
+++ models/user.js
@@ -3,3 +3,13 @@ var User = function(){
     this.password;
     this.email;
 };
+
+store.hgetall('user:'+ username, function(err, results) {
+	var user = {
+		username:username,
+		salt:results.salt,
+		pwd:results.pwd,
+		email:results.email,
+	};
+	console.log('username: ' + user.username + ' salt : ' + user.salt + ' pwd: '+ user.pwd +' email: ' +user.email);
+});
