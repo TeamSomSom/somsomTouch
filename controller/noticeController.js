1cc0180f83276a773868f9a96331dbe9dad126d3
LimAh Lee <rima361@naver.com>
2018-03-10 16:09:59 +0900

    Notice Create : select option

--- controller/noticeController.js
+++ controller/noticeController.js
@@ -16,12 +16,20 @@ exports.create = function(req, res) {
 
     var title = req.body.title;
     var content = req.body.content;
+<<<<<<< Updated upstream
     var category = req.body.category;
     var date = new Date().toISOString().substring(0, 10);
 
     console.log('title : ' + title + ', content : ' + content + ', date : ' + date + ', category : ' + category + ', date : ' + date);
     store.hmset('notice:' + (++id), 'title', title, 'content', content, 'date', date, 'category', category);
 
+=======
+    var category = req.body.notice_cate;
+    var date = new Date().toISOString().substring(0, 10);
+
+    console.log('title : ' + title + ', content : ' + content + ', date : ' + date+ ', category: ' + category); //+ ', category : ' + category +
+    store.hmset('notice:' + (++id), 'title', title, 'content', content, 'date', date, 'category', category);
+>>>>>>> Stashed changes
     res.redirect('../notice.html');
 };
 
@@ -31,9 +39,7 @@ exports.create = function(req, res) {
 *************************************************************************/
 
 exports.read = function(req, res){
-    console.log('/process/read 처리함');
-
-
+    console.log('/notice/read 처리함');
 
 };
 
@@ -43,9 +49,7 @@ exports.read = function(req, res){
 *************************************************************************/
 
 exports.update = function(req, res){
-    console.log('/process/update 처리함');
-
-
+    console.log('/notice/update 처리함');
 
 };
 
@@ -53,6 +57,7 @@ exports.update = function(req, res){
 /*********************************************************************** 
  *                             Notice Delete					   
 *************************************************************************/
+<<<<<<< Updated upstream
 
 exports.delete = function(req, res){
     console.log('/process/delete 처리함');
@@ -65,3 +70,15 @@ exports.delete = function(req, res){
 
 
 
+=======
+// 자기가 쓴 글 만 지울 수 있도록 
+exports.delete = function(req, res){
+    console.log('/notice/delete 처리함');
+    var userId = req.user.username;
+    var postId = req.body.postId;
+
+    store.del('notice:'+ postId, function(req, res){
+        res.redirect('../notice.html');
+    });
+};
+>>>>>>> Stashed changes
